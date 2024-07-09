6a3e350ad698aa9ccf119af79b9a442bbb1a7c0b
janswit <126093570+janswit@users.noreply.github.com>
2024-07-09 20:37:58 +0200

    Update SQL_Analysis_Jan_Switkowski_Query_HW.sql

--- SQL_Analysis_Jan_Switkowski_Query_HW.sql
+++ SQL_Analysis_Jan_Switkowski_Query_HW.sql
@@ -11,8 +11,8 @@ GROUP BY p.prod_category
  
 -- Calculate the average sales quantity by region for a particular product
 SELECT AVG(s.quantity_sold) AS avg_sales_quant_in_fiscal_month,
-		p.prod_name,
-		c2.country_region
+	p.prod_name,
+	c2.country_region
 FROM sh.sales s
 JOIN sh.customers c ON s.cust_id = c.cust_id 
 JOIN sh.countries c2 ON c.country_id = c2.country_id 
@@ -24,8 +24,8 @@ GROUP BY p.prod_name, c2.country_region;
 -- Find the top five customers with the highest total sales amount
 
 SELECT c.cust_id,
-		COALESCE(cust_first_name) || ' ' || COALESCE(cust_last_name) AS full_name,
-		sum(quantity_sold * amount_sold) AS total_sales
+	COALESCE(cust_first_name) || ' ' || COALESCE(cust_last_name) AS full_name,
+	sum(quantity_sold * amount_sold) AS total_sales
 FROM sh.customers c 
 JOIN sh.sales s
 	ON c.cust_id = s.cust_id 
